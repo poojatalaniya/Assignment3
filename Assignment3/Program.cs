f4a7244a99415bd852fe7e0de403c98e59b5739e
Pooja Talaniya <pooja@FLEXI>
2024-03-22 17:07:08 -0400

    Vehicle Class Created

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,3 +1,28 @@
 ﻿//Assignment3
 //By Pooja Talaniya, Student ID-8904569, E-mail-Ptalaniya4569@conestogac.on.ca
 //Beginning of the code
+using Assignment3;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating instances of vehicles
+        Car car = new Car
+        {
+            
+        };
+
+        Truck truck = new Truck
+        {
+            
+        };
+
+        Motorcycle motorcycle = new Motorcycle
+        {
+           
+        };
+
+    }
+}
---
+++ Assignment3/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public double RentalPrice { get; set; }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+    }
+}
