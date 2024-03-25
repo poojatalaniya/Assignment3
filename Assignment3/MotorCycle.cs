dff52590fa6d6911cc08d50fc8b65beca7efeec8
Pooja Talaniya <pooja@FLEXI>
2024-03-25 12:51:40 -0400

    MotorCycle Class

--- Assignment3/MotorCycle.cs
+++ Assignment3/MotorCycle.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Assignment3
+public class Motorcycle : Vehicle
 {
-    internal class MotorCycle
+    public int EngineCapacity { get; set; }
+    public string FuelType { get; set; }
+    public bool HasFairing { get; set; }
+
+    public override void DisplayDetails()
     {
+        base.DisplayDetails();
+        Console.WriteLine($"Engine Capacity: {EngineCapacity}, Fuel Type: {FuelType}, Has Fairing: {HasFairing}");
     }
 }
