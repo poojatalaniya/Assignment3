8f7366c8aa8c9636a43a7e40f5ae39a345f99bc9
Pooja Talaniya <pooja@FLEXI>
2024-03-25 15:38:20 -0400

    RentalAgency Class Created

--- Assignment3/RentalAgency.cs
+++ Assignment3/RentalAgency.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Assignment3
+public class RentalAgency
 {
-    internal class RentalAgency
+    private Vehicle[] Fleet { get; set; }
+    public double TotalRevenue { get; private set; }
+
+    public RentalAgency(int capacity)
     {
+        Fleet = new Vehicle[capacity];
+    }
+
+    public void AddVehicle(Vehicle vehicle, int index)
+    {
+        Fleet[index] = vehicle;
+    }
+
+    public void RemoveVehicle(int index)
+    {
+        Fleet[index] = null;
+    }
+
+    public void RentVehicle(int index, int days)
+    {
+        if (Fleet[index] != null)
+        {
+            TotalRevenue += Fleet[index].RentalPrice * days;
+            Console.WriteLine($"Vehicle {Fleet[index].Model} rented for {days} days. Total cost: {Fleet[index].RentalPrice * days}");
+            Fleet[index] = null;
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
     }
 }
